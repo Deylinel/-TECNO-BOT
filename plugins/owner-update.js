b95b75f71e959aaadd272a5a949d7a6519b51dfa
Deylinel <deylibaquedano40@gmail.com>
2024-12-25 11:23:21 -0600

    Actualizar owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -8,9 +8,9 @@ try {
 const stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''));
 let messager = stdout.toString()
 
-if (messager.includes('🌹 Ya estoy actualizada.')) messager = '🌹 Ya estoy actualizada a la última versión.'
+if (messager.includes('⚙️ Ya estoy actualizado.')) messager = '🚀 Ya estoy actualizado a la última versión⚙️.'
 
-if (messager.includes('🍒 Actualizando.')) messager = '🍒 Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
+if (messager.includes('🚀 Actualizando.')) messager = '⚙️ Procesando, espere un momento mientras me actualizo.\n\n' + stdout.toString()
 conn.reply(m.chat, messager, m, rcanal,)
 
 } catch { 
@@ -25,7 +25,7 @@ return null
 }
 return '*→ ' + line.slice(3) + '*'}).filter(Boolean)
 if (conflictedFiles.length > 0) {
-const errorMessage = `💭 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
+const errorMessage = `⚠️ Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.\n\n✰ *ARCHIVOS EN CONFLICTO*\n\n${conflictedFiles.join('\n')}`
 await conn.reply(m.chat, errorMessage, m, rcanal,)
 }
 }
